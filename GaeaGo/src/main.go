a05285dc19ee0d938dee4ad801dc1c8068ca2662
ruochenhua <ruochen.hua@gmail.com>
2018-10-21 19:02:13 +0800

    add rc graphics package

--- GaeaGo/src/main.go
+++ GaeaGo/src/main.go
@@ -1,73 +1,27 @@
 package main
 
 import (
-	"log"
-	"runtime"
-
-	"github.com/go-gl/gl/v4.6-core/gl"
-	"github.com/go-gl/glfw/v3.2/glfw"
+	"./rcgraphics"
 )
 
-func init() {
-	// This is needed to arrange that main() runs on main thread.
-	// See documentation for functions that are only allowed to be called from the main thread.
-	runtime.LockOSThread()
-}
-
-const (
-	width  = 500
-	height = 500
+var (
+	triangle = []float32{
+		0, 0.5, 0, // top
+		-0.5, -0.5, 0, // left
+		0.5, -0.5, 0, // right
+	}
 )
-
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 6)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(width, height, "GO_RC", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	window.MakeContextCurrent()
-	return window
-}
-
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	log.Println("OpenGL version", version)
-
-	prog := gl.CreateProgram()
-	gl.LinkProgram(prog)
-
-	return prog
-}
-
-func draw(window *glfw.Window, program uint32) {
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	gl.UseProgram(program)
-
-	glfw.PollEvents()
-	window.SwapBuffers()
-}
 
 func main() {
-	window := initGlfw()
-	defer glfw.Terminate()
+	window := rcgraphics.InitGlfw()
+	prog := rcgraphics.InitOpenGL()
+	vao := rcgraphics.CreateVao(triangle)
+	// window := initGlfw()
+	// defer glfw.Terminate()
 
-	program := initOpenGL()
+	// program := initOpenGL()
 	for !window.ShouldClose() {
 		//todo
-		draw(window, program)
+		rcgraphics.Draw(vao, window, prog)
 	}
 }
